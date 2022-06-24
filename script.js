40c6c51aba310749a171a27c9ad23503c6c8c6ad
daikishiroma <wli34012@gmail.com>
2022-06-24 18:48:13 +0900

    “vta”

---
+++ script.js
@@ -0,0 +1,26 @@
+const mySwiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+
+    autoplay: { // 自動再生させる
+        delay: 3000, // 次のスライドに切り替わるまでの時間（ミリ秒）
+        disableOnInteraction: false, // ユーザーが操作しても自動再生を止めない
+        waitForTransition: false, // アニメーションの間も自動再生を止めない（最初のスライドの表示時間を揃えたいときに）
+    },
+});
