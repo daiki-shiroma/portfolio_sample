681cd5af7eabbb2ce1d74f45ce4aa623331a481b
daiki-shiroma <94623074+daiki-shiroma@users.noreply.github.com>
2022-06-26 21:18:38 +0900

    Delete ModelPrinter.java

--- ModelPrinter.java
+++
@@ -1,9 +0,0 @@
-package lifegame;
-import javax.swing.JPanel;
-public class ModelPrinter extends JPanel implements BoardListener {
-	
-	public void updated(BoardModel model) {
-        model.printForDebug();
-              
-}	
-}
